46c7412cdd371f25bb99d4f416e6cbacedb5282c
sumitdas16 <sumitdasnaity@gmail.com>
2024-08-06 12:16:15 +0530

    Products Details page added

---
+++ public/assets/Products/Card.js
@@ -0,0 +1,33 @@
+import image1 from "./image1.png";
+import image2 from "./image2.png";
+import image3 from "./image3.png";
+import image4 from "./image4.png";
+
+const Data = [
+    {
+        "id": 1,
+        "title": "ADIDAS 4DFWD X PARLEY RUNNING SHOES",
+        "price": "$125",
+        "image": image1
+    },
+    {
+        "id": 2,
+        "title": "ADIDAS 4DFWD X PARLEY BLAZING SHOES",
+        "price": "$155",
+        "image": image2
+    },
+    {
+        "id": 3,
+        "title": "ADIDAS 4DFWD X PARLEY SILVER SHOES",
+        "price": "$225",
+        "image": image3
+    },
+    {
+        "id": 4,
+        "title": "ADIDAS 4DFWD X PARLEY OFFWHITE SHOES",
+        "price": "$380",
+        "image": image4
+    }
+]
+
+export default Data
--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import Hero from './components/Hero/Hero'
 import Cards from './components/Cards/Cards'
 import Categories from './components/Categories/Categories'
 import Review from './components/Reviews/Review'
+import Products from './components/Products/Products'
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Hero />
-      <Cards />
-      <Categories />
-      <Review />
+      <Products/>
     </div>
   )
 }
