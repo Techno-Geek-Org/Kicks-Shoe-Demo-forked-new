5c56f4ac7373f75411c405d31f160c970fd1d6ba
Kuntal-Gain <work.kuntalgain@gmail.com>
2024-08-07 13:19:46 +0530

    [ISSUE#01] REGISTRATION LOGIC ERROR

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,5 +5,5 @@ dotenv.config();
 
 export default {
     port: process.env.PORT || 6969,
-    mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/',
+    mongoURI: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/kicks-db',
 };
